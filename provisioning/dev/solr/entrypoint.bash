b8b7dc1e53c22614fa76efbaeae3545e50be4cc5
Vincent Guyard <vguyard@onaxis.cl>
2020-04-27 02:52:20 -0400

    First Initial Commit

---
+++ provisioning/dev/solr/entrypoint.bash
@@ -0,0 +1,47 @@
+#!/usr/bin/env bash
+
+# Check if missing template folder
+DESTINATION_EZ="/ezsolr/server/ez"
+DESTINATION_TEMPLATE="$DESTINATION_EZ/template"
+if [ ! -d ${DESTINATION_TEMPLATE} ]; then
+    cd $PROJECTMAPPINGFOLDER/ezplatform
+    mkdir -p $DESTINATION_TEMPLATE
+    cp -R vendor/ezsystems/ezplatform-solr-search-engine/lib/Resources/config/solr/* $DESTINATION_TEMPLATE
+fi
+
+mkdir -p /ezsolr/server/ez
+if [ ! -f /ezsolr/server/ez/solr.xml ]; then
+    cp /opt/solr/server/solr/solr.xml /ezsolr/server/ez
+    cp /opt/solr/server/solr/configsets/basic_configs/conf/{currency.xml,solrconfig.xml,stopwords.txt,synonyms.txt,elevate.xml} /ezsolr/server/ez/template
+    sed -i.bak '/<updateRequestProcessorChain name="add-unknown-fields-to-the-schema">/,/<\/updateRequestProcessorChain>/d' /ezsolr/server/ez/template/solrconfig.xml
+    sed -i -e 's/<maxTime>${solr.autoSoftCommit.maxTime:-1}<\/maxTime>/<maxTime>${solr.autoSoftCommit.maxTime:20}<\/maxTime>/g' /ezsolr/server/ez/template/solrconfig.xml
+    sed -i -e 's/<dataDir>${solr.data.dir:}<\/dataDir>/<dataDir>\/opt\/solr\/data\/${solr.core.name}<\/dataDir>/g' /ezsolr/server/ez/template/solrconfig.xml
+fi
+
+SOLR_CORES=${SOLR_CORES:-collection1}
+CREATE_CORES=false
+
+for core in $SOLR_CORES
+do
+    if [ ! -d /ezsolr/server/ez/${core} ]; then
+        CREATE_CORES=true
+        echo "Found missing core: ${core}"
+    fi
+done
+
+if [ "$CREATE_CORES" = true ]; then
+    echo "Start solr on background to create missing cores"
+    /opt/solr/bin/solr -s /ezsolr/server/ez
+
+    for core in $SOLR_CORES
+    do
+        if [ ! -d /ezsolr/server/ez/${core} ]; then
+            /opt/solr/bin/solr create_core -c ${core}  -d /ezsolr/server/ez/template
+            echo "Core ${core} created."
+        fi
+    done
+    echo "Stop background solr"
+    /opt/solr/bin/solr stop
+fi
+
+/opt/solr/bin/solr -s /ezsolr/server/ez -f
